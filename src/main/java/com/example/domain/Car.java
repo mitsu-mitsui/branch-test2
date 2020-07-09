8516602f7228ef28c48c54ee4da1f49b5c4ee36b
HirotoKitamura <hiroto.kitamura@rakus.co.jp>
2020-07-09 12:21:31 +0900

    車のスピードを表す変数を実装

--- src/main/java/com/example/domain/Car.java
+++ src/main/java/com/example/domain/Car.java
@@ -1,5 +1,14 @@
 package com.example.domain;
 
 public class Car {
+	private int speed = 1755;
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
 
 }
